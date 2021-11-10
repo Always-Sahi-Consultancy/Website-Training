c14f1ddbd88d185049dddea9e4bd9c7d97a95003
Akhil-A0402 <akhil.a.nair760@gmail.com>
2021-11-10 21:58:52 +0530

    Javascript Day 1

---
+++ Training/Task, Demo and Solutions/Demo/javascript/Day01/javascript/index.js
@@ -0,0 +1,20 @@
+console.log('Hello');
+var my_var; // ES5
+my_var = "Healfanglk";
+console.log(my_var);
+my_var = 151;
+console.log(my_var);
+
+// let, const
+
+let my_var1 = 121;
+my_var1 = 112412;
+console.log(my_var1);
+const my_var2 = 1512;
+// my_var2 = 235235;
+console.log(my_var2);
+
+console.log("Hello");
+console.log(1);
+console.log(true);
+console.log(null);
